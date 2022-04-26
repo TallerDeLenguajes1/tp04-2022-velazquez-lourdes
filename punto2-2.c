aafa13680e1316f0534e23a3ed17d66631450870
velazquez-lourdes <lourdesailenv@gmail.com>
2022-04-25 22:04:21 -0300

    Punto 2, apartado 1,2,3,4

---
+++ punto2-2.c
@@ -0,0 +1,101 @@
+#include<string.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+typedef struct 
+{
+    int TareaID; //Numerado en ciclo iterativo
+    char *Descripcion;
+    int Duracion; // entre 10 -100
+}Tarea;
+
+void CargarTareas (Tarea **buff, int a);
+void MoverTarea (Tarea **buff, int a , Tarea **Trealizada);
+void MostrarTarea (Tarea **buff, int a);
+
+int main ()
+{
+    srand(time(NULL));
+    int CantTarea =0;
+    Tarea **tarea;
+    Tarea **tarea_realizada;
+    tarea=(Tarea**)malloc(sizeof(Tarea*)*CantTarea);
+    tarea_realizada = (Tarea**)malloc(sizeof(Tarea*)*CantTarea);
+   while (CantTarea == 0)
+   {
+    printf("Ingrese la canrtidad de tareas a cargar: ");
+    scanf("%d", &CantTarea);
+    fflush(stdin);
+   }
+    printf("\n_______________________Cargar Tareas___________________________\n");
+    CargarTareas(tarea , CantTarea);
+     printf("\n_______________________Mover Tareas___________________________\n");
+    MoverTarea(tarea, CantTarea, tarea_realizada);
+    printf("\n_______________________Tareas Realizadas________________________\n");
+    MostrarTarea(tarea_realizada, CantTarea);
+    printf("\n_______________________Tareas Pendientes________________________\n");
+    MostrarTarea(tarea, CantTarea);
+return 0;
+}
+
+void CargarTareas(Tarea **buff, int a)
+{
+   
+    for (int i = 0; i < a ; i++)
+    {
+        buff[i]=(Tarea*)malloc(sizeof(Tarea));
+        char AuxDescripcion[1000];
+        buff[i]->TareaID= 1+ i;
+        printf("Tarea numero Numero %d :", buff[i]->TareaID);
+        printf("\nDescripcion de la tarea numero %d: ", buff[i]->TareaID);
+        gets(AuxDescripcion);
+        fflush(stdin);
+        int Caracter = strlen(AuxDescripcion);
+        buff[i]->Descripcion = (char*)malloc(sizeof(char)*Caracter);
+        strcpy(buff[i]->Descripcion, AuxDescripcion);
+        buff[i]->Duracion= 10 + rand ()%100;
+        printf("\nDuracion: %d ", buff[i]->Duracion);
+        printf("\n ________________________________________________________\n");
+        Caracter= 0; 
+    } 
+}
+
+void MoverTarea (Tarea **Tpendiente, int a, Tarea **Trealizada )
+{
+    char pregunta;
+    for (int i = 0; i < a; i++)
+    {
+      printf("Tarea Numero: %d", Tpendiente[i]->TareaID);
+      printf("¿La tarea fue realizada? (s o n)  ");
+      scanf("%c" , &pregunta);
+      fflush(stdin);
+        if (pregunta == 's')
+             {
+                Trealizada [i]= (Tarea*)malloc(sizeof(Tarea));
+                Trealizada[i]->TareaID = Tpendiente[i]->TareaID;
+                Trealizada[i]->Descripcion = (char *) malloc(sizeof(char)*strlen(Tpendiente[i]->Descripcion));
+                strcpy(Trealizada[i]->Descripcion, Tpendiente[i]->Descripcion);
+                Trealizada[i]->Duracion = Tpendiente[i]->Duracion; 
+                Tpendiente[i]=NULL;
+             }
+        else 
+            {
+                Trealizada[i] = NULL;
+            }
+    }
+   
+}
+
+void MostrarTarea (Tarea **buff, int a)
+{
+    for (int i = 0; i < a; i++)
+    {
+       if (buff[i] != NULL)
+       {
+        printf("Tarea numero : %d \n ", buff[i]->TareaID);
+        printf("Descripcion de la tarea:  %s \n", buff[i]->Descripcion);
+        printf("Duracion %d : \n", buff[i]->Duracion);
+       }  
+    }
+}
